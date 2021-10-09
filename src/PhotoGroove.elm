71aa5733d1c0da498dd173dd206f050eb3eecbba
bobby3003 <roberto3003@riseup.net>
2021-10-09 11:44:59 +0700

    List.map

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -3,16 +3,24 @@ module PhotoGroove exposing (main)
 import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
+urlPrefix =
+    "http://elm-in-action.com/"
+
 view model =
     div [ class "content" ]
-    [h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "tbumbdanils" ] (List.map viewThumbnail model)
         ]
+        
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.url ) ] []
+          
+initialModel = 
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
           
 main =
-    view "no model yet"
+    view initialModel
 
