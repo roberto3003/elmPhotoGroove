812ce00742c169cce8831211500447d872887e6c
bobby3003 <roberto3003@riseup.net>
2021-10-09 20:35:37 +0700

    button

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,6 +1,6 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Browser
@@ -11,9 +11,16 @@ urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
+type alias Msg = 
+    { description : String, data : String }
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick { description = "ClickedSurpriseMe", data = "" }]
+            [ text "Surprise Me!" ]
         , div [ id "thumbnails" ] 
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
@@ -23,6 +30,7 @@ view model =
             []
         ]
         
+viewThumbnail : String -> Photo -> Html Msg        
 viewThumbnail selectedUrl thumb =
     img 
         [ src (urlPrefix ++ thumb.url)
@@ -31,7 +39,12 @@ viewThumbnail selectedUrl thumb =
         ]
         []
               
-initialModel : { photos : List { url : String }, selectedUrl : String }              
+type alias Photo = { url : String }
+
+type alias Model = { photos : List Photo, selectedUrl : String }  
+
+initialModel : Model               
+
 initialModel = 
     { photos =
         [ { url = "1.jpeg" }
@@ -42,17 +55,21 @@ initialModel =
     }
     
 
-photoArray : Array { url : String }
+photoArray : Array Photo
 photoArray =
     Array.fromList initialModel.photos
 
+update : Msg -> Model -> Model
 update msg model =
-    if msg.description == "ClickedPhoto" then
-        { model | selectedUrl = msg.data }
+    case msg.description of 
+        "ClickedPhoto" -> 
+            { model | selectedUrl = msg.data }
 
-    else 
-        model
-          
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+        _-> 
+            model
+         
 main =
     Browser.sandbox
         { init = initialModel
