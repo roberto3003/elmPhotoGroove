f2e798917d8053a2533d79b5ee7b2a9095be9150
bobby3003 <roberto3003@riseup.net>
2021-10-09 21:25:39 +0700

    message as type

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -11,15 +11,17 @@ urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
-type alias Msg = 
-    { description : String, data : String }
+type Msg  
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
 
 view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , button
-            [ onClick { description = "ClickedSurpriseMe", data = "" }]
+            [ onClick ClickedSurpriseMe ]
             [ text "Surprise Me!" ]
         , h3 [] [ text "Thumbnail Size:" ]
         , div [ id "choose-size" ]
@@ -38,14 +40,14 @@ viewThumbnail selectedUrl thumb =
     img 
         [ src (urlPrefix ++ thumb.url)
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
-        , onClick { description = "ClickedPhoto", data = thumb.url }
+        , onClick ( ClickedPhoto thumb.url )
         ]
         []
 
 viewSizeChooser : ThumbnailSize -> Html Msg
 viewSizeChooser size =
     label []
-        [ input [ type_ "radio", name "size" ] []
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
         , text (sizeToString size)
         ]
 
@@ -89,16 +91,25 @@ photoArray : Array Photo
 photoArray =
     Array.fromList initialModel.photos
 
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of 
+        Just photo ->
+            photo.url
+        Nothing ->
+            ""
+
 update : Msg -> Model -> Model
 update msg model =
-    case msg.description of 
-        "ClickedPhoto" -> 
-            { model | selectedUrl = msg.data }
+    case msg of 
+        ClickedPhoto url ->
+            { model | selectedUrl = url }
 
-        "ClickedSurpriseMe" ->
+        ClickedSize size ->
+            { model | chosenSize = size }
+                
+        ClickedSurpriseMe ->
             { model | selectedUrl = "2.jpeg" }
-        _-> 
-            model
          
 main =
     Browser.sandbox
