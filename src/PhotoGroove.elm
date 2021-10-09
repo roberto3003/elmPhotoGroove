a287ef23add6f73799a6000ee07d4cc6813ad305
bobby3003 <roberto3003@riseup.net>
2021-10-09 11:58:55 +0700

    large pics

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -9,17 +9,39 @@ urlPrefix =
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "tbumbdanils" ] (List.map viewThumbnail model)
+        , div [ id "thumbnails" ] 
+            (List.map 
+                (\photo -> viewThumbnail model.selectedUrl photo)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
         ]
         
-viewThumbnail thumb =
-    img [ src (urlPrefix ++ thumb.url ) ] []
+viewThumbnail selectedUrl thumb =
+    if selectedUrl == thumb.url then
+    img 
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        ]
+        []
+    else
+        img
+            [ src (urlPrefix ++ thumb.url) ]
+            []
           
 initialModel = 
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
           
 main =
     view initialModel
